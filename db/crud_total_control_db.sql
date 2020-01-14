2b4a91923302d7cbfe04b3841af951cb7a61754d
Gabriel Aragao <gabrielneynn@gmail.com>
2020-01-14 08:21:34 -0300

    Crud Finalizado DB incluído

---
+++ db/crud_total_control_db.sql
@@ -0,0 +1,68 @@
+CREATE DATABASE  IF NOT EXISTS `crud_total_control` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `crud_total_control`;
+-- MySQL dump 10.13  Distrib 5.7.29, for Win32 (AMD64)
+--
+-- Host: localhost    Database: crud_total_control
+-- ------------------------------------------------------
+-- Server version	5.7.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `CODIGO_USUARIO` int(11) NOT NULL AUTO_INCREMENT,
+  `TIPO_PESSOA` varchar(45) NOT NULL,
+  `NOME_CLIENTE` varchar(100) NOT NULL,
+  `RAZAO_SOCIAL` varchar(100) DEFAULT NULL,
+  `CPF_CNPJ` varchar(45) NOT NULL,
+  `ENDERECO` varchar(100) NOT NULL,
+  `NUMERO` varchar(45) DEFAULT NULL,
+  `COMPLEMENTO` varchar(45) DEFAULT NULL,
+  `CEP` varchar(45) NOT NULL,
+  `MUNICIPIO` varchar(45) NOT NULL,
+  `CIDADE` varchar(45) NOT NULL,
+  `EMAIL` varchar(100) DEFAULT NULL,
+  `TELEFONE` varchar(45) DEFAULT NULL,
+  `CELULAR` varchar(45) DEFAULT NULL,
+  `CLIENTE` tinyint(1) DEFAULT NULL,
+  `FORNECEDOR` tinyint(1) DEFAULT NULL,
+  `FUNCIONARIO` tinyint(1) DEFAULT NULL,
+  `STATUS` varchar(1) NOT NULL DEFAULT 'A',
+  PRIMARY KEY (`CODIGO_USUARIO`),
+  UNIQUE KEY `CPF_CNPJ_UNIQUE` (`CPF_CNPJ`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping events for database 'crud_total_control'
+--
+
+--
+-- Dumping routines for database 'crud_total_control'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-14  8:06:11
--- index.php
+++ index.php
@@ -12,7 +12,8 @@ if(isset($_POST['cadastrar'])){
         $tipo_de_pessoa = $_POST['tipo_de_pessoa'];
 
         //verificando se é pessoa fisica e se enviou CPF
-        if($tipo_de_pessoa == 'fisica' && !empty($_POST['cpf'])){
+        if($tipo_de_pessoa == 'fisica'){
+            if(!empty($_POST['cpf'])){
                 $nome = addslashes($_POST['nome']);
                 $razao_social = "";
                 $endereco = addslashes($_POST['endereco']);
@@ -25,58 +26,83 @@ if(isset($_POST['cadastrar'])){
                 $celular = addslashes($_POST['celular']);
                 $numero = addslashes($_POST['numero']);
                 $complemento = addslashes($_POST['complemento']);
-        }else{
-            echo "<script>alert('O preenchimento do CPF é obrigatório.')</script>";
-        }
 
-        //verificando se é jurídica e se enviou CNPJ e Razão social
-        if($tipo_de_pessoa == 'juridica'){
+                //verificando se foi enviado pelo menos um tipo de usuario e salvando-o;
 
-            if (empty($_POST['razao_social']) || empty($_POST['cnpj'])){
+                if(!empty($_POST['cliente']) || !empty($_POST['fornecedor']) || !empty($_POST['funcionario'])){
+
+                    $cliente = 0; //default
+                    $fornecedor = 0; //default
+                    $funcionario = 0; //default
+
+                    if(isset($_POST['cliente'])){$cliente = $_POST['cliente'];}
+                    if(isset($_POST['fornecedor'])){$fornecedor = $_POST['fornecedor'];}
+                    if(isset($_POST['funcionario'])){$funcionario = $_POST['funcionario'];}
+
+
+                    //Se chegou até aqui, tudo foi enviado corretamente - Inserir no Banco
+
+                    if($usuario->inserirUsuario($tipo_de_pessoa, $nome, $cpf_cnpj, $razao_social, $endereco, $numero, $complemento, $cep, $municipio, $cidade, $email, $telefone, $celular, $cliente, $fornecedor, $funcionario)){
+                        echo "<script>alert('Usuário inserido com sucesso!')</script>";
+                        echo "<script>location.href='index.php'</script>"; //redirecionando com javascript para ver o alert e para atualizar número do código.
+                        
+                    }else{
+                        echo "<script>alert('CPF/CNPJ já cadastrado.')</script>";
+                    }
+
+                }else{
+                    echo "<script>alert('Pelo menos uma opção de tipo de usuario deve ser selecionada.')</script>";
+                }
+                }else{
+                    echo "<script>alert('O preenchimento do CPF é obrigatório.')</script>";
+                }
+        
+            //Se chegou até aqui é jurídica verificando se enviou CNPJ e Razão social
+
+        }else if (empty($_POST['razao_social']) || empty($_POST['cnpj'])){
                 
-                echo "<script>alert('Os campos Razão Social e CNPJ são obrigatórios para pessoas Jurídicas.')</script>";
-
-            }else{
-                $razao_social = addslashes($_POST['razao_social']);
-                $nome = addslashes($_POST['nome']);
-                $endereco = addslashes($_POST['endereco']);
-                $cep = addslashes($_POST['cep']);
-                $municipio = addslashes($_POST['municipio']);
-                $cidade = addslashes($_POST['cidade']);
-                $cpf_cnpj = addslashes($_POST['cnpj']);
-                $email = addslashes($_POST['email']);
-                $telefone = addslashes($_POST['telefone']);
-                $celular = addslashes($_POST['celular']);
-                $numero = addslashes($_POST['numero']);
-                $complemento = addslashes($_POST['complemento']);
-            }
-        }
-
-        //verificando se foi enviado pelo menos um tipo de usuario e salvando-o;
-
-        if(!empty($_POST['cliente']) || !empty($_POST['fornecedor']) || !empty($_POST['funcionario'])){
-
-            $cliente = 0; //default
-            $fornecedor = 0; //default
-            $funcionario = 0; //default
-
-            if(isset($_POST['cliente'])){$cliente = $_POST['cliente'];}
-            if(isset($_POST['fornecedor'])){$fornecedor = $_POST['fornecedor'];}
-            if(isset($_POST['funcionario'])){$funcionario = $_POST['funcionario'];}
-
-
-            //Se chegou até aqui, tudo foi enviado corretamente - Inserir no Banco
-
-            if($usuario->inserirUsuario($tipo_de_pessoa, $nome, $cpf_cnpj, $razao_social, $endereco, $numero, $complemento, $cep, $municipio, $cidade, $email, $telefone, $celular, $cliente, $fornecedor, $funcionario)){
-                echo "<script>alert('Usuário inserido com sucesso!')</script>";
-                echo "<script>location.href='index.php'</script>"; //redirecionando com javascript para ver o alert e para atualizar número do código.
-                
-            }else{
-                echo "<script>alert('CPF/CNPJ já cadastrado.')</script>";
-            }
+            echo "<script>alert('Os campos Razão Social e CNPJ são obrigatórios para pessoas Jurídicas.')</script>";
 
         }else{
-            echo "<script>alert('Pelo menos uma opção de tipo de usuario deve ser selecionada.')</script>";
+            $razao_social = addslashes($_POST['razao_social']);
+            $nome = addslashes($_POST['nome']);
+            $endereco = addslashes($_POST['endereco']);
+            $cep = addslashes($_POST['cep']);
+            $municipio = addslashes($_POST['municipio']);
+            $cidade = addslashes($_POST['cidade']);
+            $cpf_cnpj = addslashes($_POST['cnpj']);
+            $email = addslashes($_POST['email']);
+            $telefone = addslashes($_POST['telefone']);
+            $celular = addslashes($_POST['celular']);
+            $numero = addslashes($_POST['numero']);
+            $complemento = addslashes($_POST['complemento']);
+
+            //verificando se foi enviado pelo menos um tipo de usuario e salvando-o;
+
+            if(!empty($_POST['cliente']) || !empty($_POST['fornecedor']) || !empty($_POST['funcionario'])){
+
+                $cliente = 0; //default
+                $fornecedor = 0; //default
+                $funcionario = 0; //default
+
+                if(isset($_POST['cliente'])){$cliente = $_POST['cliente'];}
+                if(isset($_POST['fornecedor'])){$fornecedor = $_POST['fornecedor'];}
+                if(isset($_POST['funcionario'])){$funcionario = $_POST['funcionario'];}
+
+
+                //Se chegou até aqui, tudo foi enviado corretamente - Inserir no Banco
+
+                if($usuario->inserirUsuario($tipo_de_pessoa, $nome, $cpf_cnpj, $razao_social, $endereco, $numero, $complemento, $cep, $municipio, $cidade, $email, $telefone, $celular, $cliente, $fornecedor, $funcionario)){
+                    echo "<script>alert('Usuário inserido com sucesso!')</script>";
+                    echo "<script>location.href='index.php'</script>"; //redirecionando com javascript para ver o alert e para atualizar número do código.
+                    
+                }else{
+                    echo "<script>alert('CPF/CNPJ já cadastrado.')</script>";
+                }
+
+            }else{
+                echo "<script>alert('Pelo menos uma opção de tipo de usuario deve ser selecionada.')</script>";
+            }
         }
 
     }else{
